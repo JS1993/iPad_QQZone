f46b71e1d76585c81d7ecf5836a6ba65e6939beb
susu <28025920@qq.com>
2016-06-03 19:58:41 +0800

    上传素材

---
+++ iPad_QQZone/iPad_QQZone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPad_QQZone
+//
+//  Created by  江苏 on 16/6/3.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iPad_QQZone/iPad_QQZoneTests/iPad_QQZoneTests.m
@@ -0,0 +1,39 @@
+//
+//  iPad_QQZoneTests.m
+//  iPad_QQZoneTests
+//
+//  Created by  江苏 on 16/6/3.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iPad_QQZoneTests : XCTestCase
+
+@end
+
+@implementation iPad_QQZoneTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
